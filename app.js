9547dcf8b5707e3f6c69b17e052b3274a94e0ba8
Shubham Gautam <sg2199203@gmail.com>
2020-12-20 11:21:14 +0530

    service-worker installed

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 if("serviceWorker" in navigator) {
     //regestering service worker
     //service worker is a file which is used to catch data
-    navigator.serviceWorker.register("/service-worker.js")
+    navigator.serviceWorker.register("./service-worker.js")
+    .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    })
+    .catch(err => {
+        //registration failed 
+        console.log('ServiceWorker registration failed: ', err);
+    });
+} 
+else {
+    console.log('No service-worker on this browser');
 }
