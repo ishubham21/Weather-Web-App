c96d25585d3c1e330fd26578b01492431592a651
Shubham Gautam <sg2199203@gmail.com>
2020-12-20 12:39:18 +0530

    Update

--- app.js
+++ app.js
@@ -14,6 +14,37 @@ else {
     console.log('No service-worker on this browser');
 }
 
+
+//add to home screen button
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = 'block';
+
+    addBtn.addEventListener('click', (e) => {
+        // hide our user interface that shows our A2HS button
+        addBtn.style.display = 'none';
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('Installing...');
+            } else {
+                console.log('As you wish.');
+            }
+            deferredPrompt = null;
+        });
+    });
+});
+
 function getWeather(city){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&apikey=946c870db6ca82b017d54a8aa62c349a`)
         .then(weatherObj => weatherObj.json())
